5fb431335c179a07eb4317526d85ecfc90c6ca0e
Mrityunjay Roy <royr91599@gmail.com>
2026-07-13 22:49:14 +0530

    added types

---
+++ internal/room/types.go
@@ -0,0 +1,64 @@
+package room
+
+import (
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+type Message struct {
+	ID int `json:"id"`
+	From string `json:"from"`
+	Content string `json:"content"`
+	Timestamp time.Time `json:"timestamp"`
+	Channel string `json:"channel"`
+}
+
+type Client struct {
+	conn net.Conn
+	username string
+	outgoing chan string
+	lastActive time.Time
+	messageSent int
+	messageRecv int
+	isSlowClient bool
+	reconnectToken string
+	mu sync.Mutex
+}
+
+type DirectMessage struct {
+	toClient *Client
+	message string
+}
+
+type SessionInfo struct {
+	Username string
+	ReconnectToken string
+	LastSeen time.Time
+	CreatedAt time.Time
+}
+
+type Room struct {
+	join chan *Client
+	leave chan *Client
+	broadcast chan string
+	listUsers chan *Client
+	directMessage chan DirectMessage
+	
+	clients map[*Client]bool
+	mu sync.Mutex
+	totalMesages int
+	startTime time.Time
+
+	messages []Message
+	messageMu sync.Mutex
+	nextMessageID int
+	
+	walFile *os.File
+	walMu sync.Mutex
+	dataDir string
+
+	sessions map[string]*SessionInfo
+	sessionMu sync.Mutex
+}
