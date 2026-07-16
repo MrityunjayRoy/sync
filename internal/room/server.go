4752ecd31a30c2f0f034ecc0e7e7672a3821ddc2
Mrityunjay Roy <royr91599@gmail.com>
2026-07-17 00:00:03 +0530

    added event loop and server run logic

--- internal/room/server.go
+++ internal/room/server.go
@@ -2,6 +2,10 @@ package room
 
 import (
 	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -48,3 +52,79 @@ func (room *Room) periodicSnapshot() {
 		}
 	}
 }
+
+func (room *Room) Run(){
+	fmt.Println("Room started...")
+	go room.cleanUpInactiveClient()
+
+	for {
+		select {
+		case client := <- room.join:
+			room.handleJoin(client)
+
+		case client := <- room.leave:
+			room.handleLeave(client)
+
+		case message := <- room.broadcast:
+			room.handleBroadcast(message)
+
+		case client := <- room.listUsers:
+			room.sendUserList(client)
+
+		case dm := <- room.directMessage:
+			room.handleDirectMessage(dm)
+		}
+	}
+}
+
+func (room *Room) Shutdown() {
+	fmt.Println("\n Shutting down...")
+	if err := room.createSnapshot(); err != nil {
+		fmt.Println("Error creating Final snapshot: %v\n", err)
+	}
+
+	if room.walFile != nil {
+		room.walFile.Close()
+	}
+
+	fmt.Println("Shutdown completed gracefully!")
+}
+
+func RunServer() {
+	room, err := NewRoom("./chatdata") 
+		if err != nil {
+			fmt.Printf("Failed to initialize: %v\n", err)
+		}
+		defer room.Shutdown()
+
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+		go func() {
+			 <- sigChan
+			 fmt.Println("\n Recieved shutdown signal")
+			 room.Shutdown()
+			 os.Exit(0)
+		}()
+
+		go room.Run()
+
+		listener, err := net.Listen("tcp", ":9000")
+		if err != nil {
+			fmt.Printf("Error starting server: %v\n", err)
+		}
+		defer listener.Close()
+
+		fmt.Println("Server Started on :9000")
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connections: %v\n", err)
+				continue
+			}
+			fmt.Println("New Connection from", conn.RemoteAddr())
+			go handleClient(conn, room)
+		}
+}
+
