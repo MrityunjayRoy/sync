ae9e372308f6d334a57c32997af87b2972a289de
Mrityunjay Roy <royr91599@gmail.com>
2026-07-27 14:29:51 +0530

    feat(client): added client setup

---
+++ internal/room/client.go
@@ -0,0 +1,53 @@
+package room
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func StartClient() {
+	conn, err := net.Dial("tcp", ":9000")
+	if err != nil {
+		fmt.Printf("Error connecting: ", err)
+		return
+	}
+
+	defer conn.Close()
+
+	fmt.Printf("Connected to chat server")
+	
+	//Background goroutine: read from server
+	go func() {
+		reader := bufio.NewReader(conn)
+		for {
+			message, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Printf("Disconnected from server")
+				os.Exit(0)
+			}
+
+			//clear current prompt line and print message
+			fmt.Print("\r", message)
+			fmt.Print(">> ")
+		}
+	}()
+
+	// main goroutine read from stdin
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Welcome to the server")
+
+	for {
+		fmt.Print(">> ")
+		message, _ := inputReader.ReadString('\n')
+		message = strings.TrimSpace(message)
+
+		if message == "" {
+			continue
+		}
+
+		conn.Write([]byte(message + "\n"))
+	}
+}
